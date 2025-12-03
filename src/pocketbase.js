352f20a23c46295e5e59f0bb853e3b225e36fb30
pphmtrungkin <phmtrungkinn@gmail.com>
2025-12-03 17:26:51 +1100

    Fix Bug

--- src/pocketbase.js
+++ src/pocketbase.js
@@ -1,6 +1,7 @@
 import PocketBase from "pocketbase";
 
-const pb = new PocketBase(import.meta.env.VITE_POCKETBASE_URL);
+const baseUrl = import.meta.env.VITE_POCKETBASE_URL || 'http://127.0.0.1:8090';
+const pb = new PocketBase(baseUrl);
 
 pb.autoCancellation(false);
 
